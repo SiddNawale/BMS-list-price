b12ca92250028c174576cb1064ebca0603e2b9db
alireza-hojjati <109696490+alireza-hojjati@users.noreply.github.com>
2023-03-07 07:33:57 -0500

    draft header template

---
+++ sql_header_template.sql
@@ -0,0 +1,14 @@
+-------------------------------------------------------------------------------
+-- Author       name
+-- Created      date
+-- Project
+-- Purpose      description of the business/technical purpose
+--              using multiple lines as needed
+-- Input        - tables used, 
+                - main collection of fields used
+                - main transformation
+-- Output       - table FQDN
+                - main output
+-- 
+-- Steps        - high-level steps and main transformations
+-------------------------------------------------------------------------------
