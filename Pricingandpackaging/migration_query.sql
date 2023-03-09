27cd78ca712ff5986f11189947c0a6b2557e9dd0
alireza-hojjati <109696490+alireza-hojjati@users.noreply.github.com>
2023-03-09 12:17:22 -0500

    removed  MSB_FLAG = 1 from current monthly

--- Pricingandpackaging/migration_query.sql
+++ Pricingandpackaging/migration_query.sql
@@ -1210,15 +1210,15 @@ current_monthly as (
         end as cmp
     from
         arr_billings arr
-        left join (
-            select
-                COMPANY_ID,
-                COMPANY_NAME_ID
-            from
-                DATAIKU.DEV_DATAIKU_STAGING.PNP_COMPANY_DIM
-        ) c on c.COMPANY_ID = ARR.COMPANY_ID
-    where
-        MSB_FLAG = 1
+--         left join (
+--             select
+--                 COMPANY_ID,
+--                 COMPANY_NAME_ID
+--             from
+--                 DATAIKU.DEV_DATAIKU_STAGING.PNP_COMPANY_DIM
+--         ) c on c.COMPANY_ID = ARR.COMPANY_ID
+--     where
+--         MSB_FLAG = 1
     group by
         1,
         2,
